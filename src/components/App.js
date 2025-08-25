9bee3128d2b391464390455f4263a1bef47169c6
Akash Patel <akashpatel260603@gmail.com>
2025-08-25 17:36:34 +0530

    semidone

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,47 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import Tasks from "./Tasks"
 
+
+
+const data = [];
 const App = () => {
+  const[tasks ,setTasks] = useState(data)
+  const[value , setValue] = useState("")
   return (
     <div>
         {/* Do not remove the main div */}
+        
+        <div>
+          <p>To-Do List</p>
+        <input className="input" name="task" placeholder="Enter the task" value={value}
+         onChange={(e)=>{
+               setValue(e.target.value)
+        }} />
+        <br/>
+        <button onClick={()=>{
+              if(!value)
+              return 
+              let newtask ={
+                task : value,
+                id : tasks.length+1
+              }  
+              setTasks([...tasks , newtask])
+              
+        }}>Add Task</button>
+        </div>
+        <ul>
+          {
+            tasks.map((item)=>{
+               return (
+                <Tasks tasks={item.task} setTasks={item.setTasks} task={item}/>
+               )
+            })
+          }
+        </ul>
     </div>
+    
   )
 }
 
---
+++ src/components/Tasks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tasks = ({tasks, setTasks,}) => {
+  return (
+    <div>
+        <p>{tasks}</p>
+    </div>
+  )
+}
+
+export default Tasks
